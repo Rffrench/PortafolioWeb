c889aa02767f257b620839f570c8fe164817f7e3
Roberto Ffrench-Davis <roberto_ffrench@hotmail.com>
2020-09-02 00:54:17 -0400

    Axios working. Main site is able to send requests to microservice and add data to DBgit add .!

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,12 +1,34 @@
 // Controlador para Autenticación
 
 const User = require('../models/userModel');
+const axios = require('axios');
+const qs = require('qs');
 
 exports.getLogin = (req, res, next) => {
     res.render('auth/login', { pageTitle: 'Login Restaurante', path: '/auth/login' });
 }
 
 exports.postLogin = (req, res, next) => {
-    console.log('Funciona');
-    res.redirect('/auth/login');
+    const data = {
+        'email': "hola@hola.com",
+        'password': 123456,
+        'name': "Juan"
+    };
+    const options = {
+        method: 'POST',
+        headers: { 'content-type': 'application/x-www-form-urlencoded' },
+        data: qs.stringify(data),
+        url: 'http://localhost:5000/api/v1/auth/login',
+    };
+    axios(options)
+        .then(response => {
+            console.log(response);
+            res.redirect('/auth/login');
+            console.log('Funciona');
+        })
+        .catch(err => {
+            console.log(err);
+        })
+
+
 }
