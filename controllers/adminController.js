8ff0795fa03e46cef1e799eccf751d986cf485ae
ThomasFarias <42778501+ThomasFarias@users.noreply.github.com>
2020-10-13 23:41:51 -0300

    Ordenes de inventario
    crear y listar
    falta:
    agregar producto a la orden
    eliminar producto de la orden
    modificar cantidad de un producto de la roden
    Finalizar orden de inventario para que el administraedor la pueda aprobar

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -40,6 +40,79 @@ exports.getProductsMenu = (req, res, next) => {
         })
 }
 
+exports.getInventoryOrderForm = (req, res, next) => {   
+    const token = localStorage.getItem('token') || null;
+
+    axios.get(`${process.env.ORCHESTRATOR}/admin/products`,
+        {
+            headers: { 'Authorization': 'Bearer ' + token } 
+        })
+        .then(response => {
+            console.log(response.data);
+            res.render('warehouse/new-inventory-order', { pageTitle: 'Ordne de inventario', path: '/admin/products', successMessage: null, errorMessage: null, products:response.data.products });
+        })
+        .catch(err => {
+            sendErrors(err.response, res);
+            return; 
+
+        })
+}
+
+exports.postInventoryOrder = (req, res, next) => {
+    const [warehouseId, description] = [req.body.userId, req.body.description];
+   
+    axios.post(`${process.env.ORCHESTRATOR}/admin/inventoryOrders`,
+    {
+        warehouseId: warehouseId,
+        description: description        
+    }).then(response=> {
+        console.log(response.data);
+        res.render('warehouse/product-info', { pageTitle: 'Products', path: '/admin/products',errorMessage: errorMessage, successMessage: null})
+    })
+    .catch(err => {
+        //res.redirect('/reservations/new');
+
+  
+        const errorResponse = err.response;
+        const errorStatus = errorResponse ? errorResponse.status : 500;
+        let errorMessage;
+
+        switch (errorStatus) {
+            case 409:
+                errorMessage = '';
+                break;
+
+            default:
+                errorMessage = 'Lo sentimos, ha ocurrido un problema de servidor. Intente nuevamente más tarde';
+                break;
+        }
+
+        res.render('warehouse/product-info', { pageTitle: 'Products', path: '/admin/products', errorMessage: errorMessage, successMessage:`Se ha creaedo la orden de inventario exitosamente.` });
+        return;
+    })
+}
+
+
+exports.getInventoryOrders = (req, res, next) => {   
+    const token = localStorage.getItem('token') || null;
+
+    axios.get(`${process.env.ORCHESTRATOR}/admin/inventoryOrders`,
+        {
+            headers: { 'Authorization': 'Bearer ' + token } 
+        })
+        .then(response => {
+            console.log(response.data);
+            res.render('warehouse/inventory-orders', { pageTitle: 'Ordenes de inventario', path: '/admin/products', successMessage: null, errorMessage: null, orders:response.data.inventoryOrders});
+        })
+        .catch(err => {
+            sendErrors(err.response, res);
+            return; 
+
+        })
+}
+
+
+
 exports.getProductList = (req, res, next) => {   
     const token = localStorage.getItem('token') || null;
 
@@ -63,7 +136,7 @@ exports.putProductView = (req, res, next) => {
 
     const product = new Product(req.body.productId, req.body.name, req.body.quantity);
 
-    axios.get(`${process.env.ORCHESTRATOR}/admin/products/update`,
+    axios.get(`${process.env.ORCHESTRATOR}/admin/products`,
         {
             headers: { 'Authorization': 'Bearer ' + token } 
         })
--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -8,10 +8,16 @@ const adminController = require('../controllers/adminController');
 router.get('/tables', adminController.getTablesView);
 //router.put('/tables/:tableId', adminController.putTables); // PUT funciona gracias a method-override
 
-router.get('/products', adminController.getProductsMenu);
+router.get('/inventoryOrder/form', adminController.getInventoryOrderForm);
+router.get('/inventory-orders', adminController.getInventoryOrders);
+router.post('/inventory-order',adminController.postInventoryOrder);
+
+
+router.get('/productsMenu', adminController.getProductsMenu);
 router.get('/product/list', adminController.getProductList);
 router.post('/product/update', adminController.putProductView);
 router.post('/products', adminController.putProduct);
+
 
 
 router.get('/recipes', adminController.getRecipesMenu);
